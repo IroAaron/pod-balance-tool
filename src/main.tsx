6dcc4185f6f30dd1b3bd00fa1089f8eddd619443
Aaron <denniksez@gmail.com>
2026-07-16 14:07:44 +0300

    theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./app/App";
 
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme/theme";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>
+    </ThemeProvider>
 );
---
+++ src/theme/theme.ts
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+
+    palette: {
+
+        mode: "dark",
+
+        primary: {
+
+            main: "#5B8CFF",
+
+        },
+
+        background: {
+
+            default: "#202124",
+
+            paper: "#2B2D31",
+
+        },
+
+    },
+
+});
+
+export default theme;
