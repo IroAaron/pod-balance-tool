e2d863f9a6e0692b28f3e970041279e94f81ca5f
Aaron <denniksez@gmail.com>
2026-07-16 13:45:34 +0300

    Structure

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function App() {
+    return (
+        <Typography variant="h3">
+            Balance Tool
+        </Typography>
+    );
+}
